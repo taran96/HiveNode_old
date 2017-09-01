eca3bae739f3fb1b1c1b3f09071466cb1f882f86
Taranvir Waraich <taranvir@me.com>
2017-09-01 07:49:17 -0400

    Converted project to use a supervision tree It also removed the previous dependencies

--- mix.exs
+++ mix.exs
@@ -14,17 +14,16 @@ defmodule Hive.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Hive.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poison, "~> 3.1"},
-      {:amqp, "~> 0.3.0"},
-      {:uuid, "~> 1.1"},
-      {:logger_file_backend, "~> 0.0.10"}, 
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
