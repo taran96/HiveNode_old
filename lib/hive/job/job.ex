f21f436cd22662b12aa9a830f9cda4796f95a01d
Taranvir Waraich <taranvir@me.com>
2017-09-04 11:40:12 -0400

    Added documentation

--- lib/hive/job/job.ex
+++ lib/hive/job/job.ex
@@ -1,17 +1,38 @@
 defmodule Hive.Job do
+  @moduledoc """
+  This module is to encapsulate jobs. These functions are not meant to be run
+  directly, even though they are full capable of it. They are merely helper 
+  functions for handling jobs.
+  """
+
+
   require Logger
 
-  defstruct( 
 
+  @doc """
+  The `%Hive.Job{}` struct is to hold jobs with a user defined name
+  """
+  defstruct( 
     name: "Hello_World",
     job_name: "echo",
     args: ["Hello", "World"],
   )
 
+
+  @doc """
+  This function basically runs the given job from a `%Hive.Job{}` format. It
+  extracts the needed information and calls `Hive.Job.run/2`
+  """
   def run(%Hive.Job{name: _, job_name: job_name, args: args}) do
     run(job_name, args)
   end
 
+
+  @doc """
+  This function runs the given job. First it finds the function within 
+  `Hive.JobList` then it executes it. It returns the status and the return 
+  value.
+  """
   def run(job_name, args \\ []) do
     case get_func(Hive.JobList.__info__(:functions), job_name) do
       {:found, func} -> {:ok, apply(Hive.JobList, func, args)}
@@ -20,6 +41,9 @@ defmodule Hive.Job do
   end
 
 
+  @doc """
+  This function validate the job. Basically it checks if the job exists
+  """
   def is_valid(job_name) do
     functions = Hive.JobList.__info__(:functions)
     case get_func(functions, job_name) do
@@ -27,6 +51,7 @@ defmodule Hive.Job do
       :not_found -> false
     end
   end
+
 
   defp get_func([{func, _arity} | tail], func_name) do
     if Atom.to_string(func) == func_name do
@@ -36,10 +61,14 @@ defmodule Hive.Job do
     end
   end
 
+
   defp get_func([], _func_name) do
     :not_found
   end
 
+  @doc """
+  This functions converts a `%Hive.Job{}` to a JSON string
+  """
   def to_json(%Hive.Job{} = job) do
     case Poison.encode(job) do
       {:ok, json} -> json
@@ -47,10 +76,16 @@ defmodule Hive.Job do
     end
   end
 
+
+  @doc """
+  This function converts a JSON string to `%Hive.Job{}`
+  """
   def from_json(json_string) when is_bitstring(json_string) do
     case Poison.decode(json_string, as: %Hive.Job{}) do
       {:ok, job} -> job
       {:error, _} -> :error
     end
   end
+
+
 end
--- lib/hive/job/job_server.ex
+++ lib/hive/job/job_server.ex
@@ -1,6 +1,11 @@
 defmodule Hive.JobServer do 
+  @moduledoc """
+  This module is responsible to handle any request to run a job
+  """
+  
   use GenServer, restart: :transient
   require Logger
+
 
   def start_link(opts \\ []) do
     pid = GenServer.start_link(__MODULE__, :ok, opts)
@@ -14,11 +19,23 @@ defmodule Hive.JobServer do
     {:ok, pids}
   end
 
+
+  @doc """
+  This function is used to run a job. It runs the job synchronously, so it will block the caller. The function also returns the value of the job.
+
+  A job is executed as such:
+      iex> {:ok, pid} = Hive.JobServer.start_link()
+      iex> Hive.JobServer.run(pid, "echo", ["Hello", "World"])
+      {:ok, {:ok, "Hello World"}} 
+  """
   def run(server, job_name, args \\ []) when is_bitstring(job_name) do
     GenServer.call(server, {:run, job_name, args})  
   end
 
 
+  @doc """
+  This call handler runs the requested job and returns the return value of the function.
+  """
   def handle_call({:run, job_name, args}, _from, pids) do
     if Hive.Job.is_valid(job_name) do
       {:reply, Hive.Job.run(job_name, args), pids}
@@ -27,6 +44,10 @@ defmodule Hive.JobServer do
     end
   end
 
+
+  @doc """
+  This info handler logs and handles a failed job
+  """
   def handle_info({:DOWN, _ref, :process, pid, reason}, pids) do
     case reason do
       :normal -> Logger.info "Process " <> inspect(pid) <> " exited gracefully"
@@ -35,6 +56,10 @@ defmodule Hive.JobServer do
     {:noreply, pids}
   end
 
+
+  @doc """
+  This handler logs any message that is not caught by other handlers. Therefore it makes the message an unhandled message.
+  """
   def handle_info(msg, pids) do
     Logger.warn "Unhandled message: " <> inspect(msg)
     {:noreply, pids}
