2c183c53c36b993aa8a519d4b240865f39ab0185
Taranvir Waraich <taranvir@me.com>
2017-09-05 12:32:39 -0400

    Added Nerves.UART to dependencies and added a function to JobList Additions:     Hive.JobList.write_to_tty/2     Hive.JobList.start_nerves_uart/0
    Changes:
        Hive.MixFile.deps/0

--- lib/hive/job/job_list.ex
+++ lib/hive/job/job_list.ex
@@ -4,4 +4,24 @@ defmodule Hive.JobList do
     {:ok, "#{hello} #{world}"}
   end
 
+  def write_to_tty(tty, string) do
+    nerves_pid = start_nerves_uart()
+    case Nerves.UART.open(nerves_pid, tty) do
+      :ok ->
+        Nerves.UART.write(nerves_pid, tty)
+        Nerves.UART.close(nerves_pid)
+      {:error, error_msg} -> {:error, error_msg}
+    end
+  end
+
+  defp start_nerves_uart do
+    pid = Process.whereis(Nerves.UART)
+    if Process.whereis(Nerves.UART) == nil do
+      {:ok, new_pid} = Nerves.UART.start_link(name: Nerves.UART)
+      new_pid
+    else
+      pid
+    end
+  end
+
 end
