a908e185b33906f4af0953e51c256eceb12d4b66
Taranvir Waraich <taranvir@me.com>
2017-09-18 21:18:08 -0400

    Added deployment stuff

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Hive.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Hive",
+      source_url: "https://github.com/taran96/hive",
+      docs: [main: "Hive",
+             extras: ["README.md"]],
     ]
   end
 
@@ -26,6 +30,8 @@ defmodule Hive.Mixfile do
       {:poison, "~> 3.1"},
       {:nerves_uart, "~> 0.1"},
       {:amqp, "~> 0.3.0"},
+      {:distillery, "~> 1.5.1", runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
     ]
   end
 end
