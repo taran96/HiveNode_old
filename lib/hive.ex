42ec9e3ccd8e235ebba24192f8fa9fab7482a95c
Taranvir Waraich <taranvir@me.com>
2017-09-30 09:59:27 -0400

    Removed Hello World

--- lib/hive.ex
+++ lib/hive.ex
@@ -2,17 +2,4 @@ defmodule Hive do
   @moduledoc """
   Documentation for Hive.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Hive.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
