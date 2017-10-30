30f29c2bd7c857e9db2d493804ee4909363fa01a
Taranvir Waraich <taranvir@me.com>
2017-10-29 23:00:42 -0400

    Config file now requires environment variable and test_helper adjusted

--- config/config.exs
+++ config/config.exs
@@ -4,8 +4,10 @@ config :logger,
   backends: [{LoggerFileBackend, :file_log}]
 
 config :logger, :file_log,
-  path: "log/debug.log"
+  path: System.get_env("LOGFILE")
 
 config :hivenode,
-  connection_string:  "amqp://node:nodepass@192.168.1.175:5672",
-  node_name: "A"
+  connection_string: System.get_env("RMQ_CONNECTION_STRING"),
+  node_name: System.get_env("NODE_NAME"),
+  echo_server_host: System.get_env("ECHO_SERVER_HOST"),
+  echo_server_port: System.get_env("ECHO_SERVER_PORT")
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,16 @@
-ExUnit.start()
+# If the tests depend on an environment variable then add it to the list
+# It follows the following format:
+#   [ environment_variable: :test_tag ]
+env_vars = [
+  echo_server_host: :echo_server_required,
+  connection_string: :rmq_required,
+]
+
+
+# Gets a list of unset environment variables
+exclude = for {env, tag} <- env_vars do
+  if Application.get_env(:hivenode, env) == nil, do: tag
+end
+
+
+ExUnit.start(exclude: exclude)
