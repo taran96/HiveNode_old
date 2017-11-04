1693f3458c44fea1bdfd79af02cd41fb53c3d5ea
Taranvir Waraich <taranvir@me.com>
2017-11-04 19:44:14 -0400

    Added test for TCP Client

--- test/hivenode/tcp/client_test.exs
+++ test/hivenode/tcp/client_test.exs
@@ -1,11 +1,13 @@
 defmodule HiveNodeTest.TCPTest.ClientTest do
   use ExUnit.Case
-  alias HiveNode.TCP.Client
+  alias HiveNode.TCP.{Client, Agent}
 
   @moduletag :echo_server_required
 
   setup do
+    Agent.start_link([name: HiveNode.TCP.Agent])
     client_params = [
+      name: "echo",
       host: Application.get_env(:hivenode, :echo_server_host),
       port: Application.get_env(:hivenode, :echo_server_port),
       mode: :binary,
@@ -22,5 +24,10 @@ defmodule HiveNodeTest.TCPTest.ClientTest do
   test "Check connection", %{client: pid} do
     assert "Hello" == Client.send_message(pid, "Hello")
   end
+
+  test "Added to agent", %{client: pid} do
+    assert pid == Agent.get(HiveNode.TCP.Agent, "echo")
+  end
+
 end
 
